ddde0ab4cab64d76e78e1ea8f784f17a156ca294
igordavid19 <sr.igordavid@gmail.com>
2022-08-06 16:06:29 -0300

    Version 1.0

---
+++ src/components/no-search/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styled";
+
+const NoSearch = () => {
+  return (
+    <S.Wrapper>
+      <h1>Digite acima para pesquisar o perfil de algum usuário</h1>
+    </S.Wrapper>
+  );
+};
+
+export default NoSearch;
